f0c2aa2f98fb0c8dda741d96ff8caeedb38338bb
Aarón Durán <aaronduranwebdev@gmail.com>
2022-09-27 16:38:03 +0200

    Ejercicios tema 2

--- tema2/baraja_DuranAaron/index.php
+++ tema2/baraja_DuranAaron/index.php
@@ -3,11 +3,11 @@ function generarBaraja()
 {
     for ($i = 0; $i < 40; $i++)
     {
-        $baraja[$i] = '<img src="img/' . $i . '.png">';
+        $baraja[$i] = '<image src="img/' . $i . '.png">';
     }
     return $baraja;
 }
-$generarTabla = function ($baraja, $texto) {
+$generarTabla = function ($array, $texto) {
     $html = '<h1>' . $texto . '</h1><table>';
     for ($fila = 0; $fila < 4; $fila++)
     {
@@ -15,7 +15,7 @@ $generarTabla = function ($baraja, $texto) {
         for ($columna = 0; $columna < 10; $columna++)
         {
             $carta = (($fila * 10) + $columna);
-            $html .= '<td>' . $baraja[$carta] . '</td>';
+            $html .= '<td>' . $array[$carta] . '</td>';
         }
         $html .= '</tr>';
     }
@@ -26,4 +26,7 @@ $baraja = generarBaraja();
 echo $generarTabla($baraja, "Baraja ordenada");
 shuffle($baraja);
 echo $generarTabla($baraja, "Baraja desordenada");
+
+
+
 ?>
--- tema2/ej18_DuranAaron.php
+++ tema2/ej18_DuranAaron.php
@@ -1,7 +1,18 @@
 <?php
+function limpiarTildes(string $cadena): string
+{
+    return str_replace(array('á', 'é', 'í', 'ó', 'ú'), 
+        array('a', 'e', 'i', 'o', 'u'), $cadena);
+}
 function esPalindromo(string $cadena): bool
 {
-
-
+    $resultado = strtolower(limpiarTildes(str_replace(' ', '', $cadena)));
+    if (strcmp(strrev($resultado), $resultado) == 0)
+    {
+        return true;
+    }
+    return false;
 }
+
+var_dump(esPalindromo("Dábale arroz a la zorra el abad"));
 ?>
